0fa7bfefd0ffe764f6abfdb88e7ff66c262679fe
Deyan Dobrinov <deyan.dobrinov.ext@friendscout24.de>
2013-12-03 10:05:48 +0200

    Generate Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DasWoerterbuch::Application.config.secret_key_base = '5a8e51fdf7cae7201e4f15312a10b8cd6d269f17404e6fefa844f01605b5d226497a3f3b22ec6a6445642c8d641f85b2df5fdc5206fee68e92193e6e2e4fe0d0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DasWoerterbuch::Application.config.session_store :cookie_store, key: '_das_woerterbuch_session'
