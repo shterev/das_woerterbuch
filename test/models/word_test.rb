8803114a7c1fc0bb47689aca83d6b61c9f244555
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2013-12-20 17:57:41 +0200

    Remove whitespace

--- test/models/word_test.rb
+++ test/models/word_test.rb
@@ -6,7 +6,7 @@ class WordInheritor < Word
 end
 
 class WordTest < ActiveSupport::TestCase
-  
+
   test "that the default specifics value is an empty hash" do
     assert Word.new.specifics.kind_of?(Hash)
   end
