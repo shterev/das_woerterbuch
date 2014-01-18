3728b6ec10386e9e4170f84f9acfd297024f730c
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2014-01-18 17:39:13 +0200

    Add a method to calculate the webiste availability period in years

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def website_availability_period_in_years
+    start_year = 2014
+    current_year = Time.now.year
+
+    (start_year == current_year) ? start_year : "#{start_year} - #{current_year}"
+  end
+
 end
